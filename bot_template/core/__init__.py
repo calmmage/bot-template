40f149c2131d5c00cf64d9995640428b7fbbb518
Petr Lavrov <calmquant@gmail.com>
2023-09-18 02:45:25 +0300

    Add database usage example

--- bot_template/core/__init__.py
+++ bot_template/core/__init__.py
@@ -1,3 +1,3 @@
-from .app import TemplateApp
 from .app_config import TemplateAppConfig
 from .telegram_bot import TemplateTelegramBot
+from .app import TemplateApp
--- bot_template/core/telegram_bot.py
+++ bot_template/core/telegram_bot.py
@@ -1,6 +1,30 @@
+from aiogram import types
 from bot_base.core.telegram_bot import TelegramBot
+
+from bot_template.core.app_config import TemplateAppConfig
+from bot_template.data_model.data_model_pydantic import \
+    SaveTelegramMessageRequest
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app import TemplateApp
 
 
 class TemplateTelegramBot(TelegramBot):
-    recognized_hashtags = {"#ignore": {"ignore": True}} #
-    pass
+    _config_class = TemplateAppConfig
+    # _app_class = "TemplateApp"
+    recognized_hashtags = {"#ignore": {"ignore": True}}  #
+
+    def __init__(self, config: TemplateAppConfig, app: "TemplateApp" = None):
+        super().__init__(config, app=app)
+
+    async def chat_message_handler(self, message: types.Message):
+        message_text = await super().chat_message_handler(message)
+
+        # save message to the database
+        if self.app:
+            request = SaveTelegramMessageRequest(
+                content=message_text,
+                timestamp=message.date,
+            )
+            self.app.save_message(request)
