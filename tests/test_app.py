f74627d269bb98ce272dbf2fe158928166c591f0
Petr Lavrov <calmquant@gmail.com>
2023-09-19 03:39:03 +0300

    fix tests - add black

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,15 @@
 from bot_template.core.app import TemplateApp
 
+import pytest
+
+
+@pytest.fixture(scope='function')
+def setup_environment(monkeypatch):
+    monkeypatch.setenv('DATABASE_CONN_STR', '')
+    monkeypatch.setenv('DATABASE_NAME', 'test_db')
+    monkeypatch.setenv('TELEGRAM_BOT_TOKEN',
+                       '1234567890:aaabbbcccdd-aaabbbcccdddeee_abcdefg')
+
 
 def test_app():
     app = TemplateApp()
