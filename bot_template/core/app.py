ecf181d9d87953e10c0cc62c4836c1437588c244
Petr Lavrov <calmquant@gmail.com>
2023-10-27 10:37:51 +0300

    Add schedule example

--- bot_template/core/app.py
+++ bot_template/core/app.py
@@ -19,3 +19,28 @@ class TemplateApp(App):
         self._connect_db()
         item = TelegramMessageMongo(content=message.content, date=message.date)
         item.save()
+
+    def _schedule_jobs(self):
+        from apscheduler.triggers.interval import IntervalTrigger
+        import datetime
+
+        assert self.config.enable_scheduler
+
+        # every Sunday at 23:59
+        next_sunday = datetime.datetime.now()
+        notify_trigger = IntervalTrigger(weeks=1, start_date=next_sunday)
+        self._scheduler.add_job(
+            self._sample_job,
+            trigger=notify_trigger,
+            id="notify_users",
+            name="Notify users",
+        )
+
+    async def _sample_job(self):
+        """
+        Refresh tokens and notify users
+        :return:
+        """
+        self.logger.info("This is a sample job")
+        message = "This is a sample job"
+        await self.bot.send_safe(message, 123)
--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 # run the app
 from bot_base.data_model.mongo_utils import connect_to_db
 from bot_base.utils.logging_utils import setup_logger
-from bot_template.core.app import App
+from bot_template.core.app import TemplateApp
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # cli kwarg data_dir:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", type=str, default="app_data")
     # connect to db
     connect_to_db()
 
     # setup logger
     setup_logger()
-
-    app = App()
+    args = parser.parse_args()
+    app = TemplateApp(data_dir=args.data_dir)
     app.run()
